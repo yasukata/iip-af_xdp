c7e773e3bd6ba4f67cf5da8d19e77b3b6ba29b2e
Kenichi Yasukata <kyasukata@outlook.com>
2024-09-13 13:54:45 +0900

    build.mk cope with paths for xsk.h

--- build.mk
+++ build.mk
@@ -1,2 +1,10 @@
 CFLAGS += -D_GNU_SOURCE
-LDFLAGS += -lpthread -lnuma -lbpf
+LDFLAGS += -lpthread -lnuma
+
+ifeq ($(shell echo '#include <bpf/xsk.h>' | cpp -H -fsyntax-only 2>&1 | head -n 1 | grep 'No such file' | wc -l), 0)
+CFLAGS += -DXSK_HEADER_LIBBPF
+LDFLAGS += -lbpf
+else ifeq ($(shell echo '#include <xdp/xsk.h>' | cpp -H -fsyntax-only 2>&1 | head -n 1 | grep 'No such file' | wc -l), 0)
+CFLAGS += -DXSK_HEADER_LIBXDP
+LDFLAGS += -lxdp
+endif
