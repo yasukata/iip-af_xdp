746267623d715eef52821d3e1910b7d10022a314
Kenichi Yasukata <kyasukata@outlook.com>
2024-09-21 21:30:59 +0900

    main definition for busy poll setting

--- main.c
+++ main.c
@@ -66,6 +66,10 @@
 #define __IOSUB_BUF_ADDR_ASSERT(__iop, __addr) do { assert((((__addr) & ~(BUF_SIZE - 1)) / BUF_SIZE) < NUM_BUF); } while (0)
 #define __IOSUB_BUF_REFCNT(__iop, __addr) ((__iop)->af_xdp.buf[((__addr) & ~(BUF_SIZE - 1)) / BUF_SIZE].ref)
 
+#ifndef __IOSUB_BUSY_POLL_SOCK
+#define __IOSUB_BUSY_POLL_SOCK (1)
+#endif
+
 struct __xpb {
 	uint64_t addr;
 	uint16_t len;
@@ -772,17 +776,19 @@ static void *__thread_fn(void *__data)
 							};
 							assert(!xsk_socket__create(&xsk, __iosub_ifname, ti->id, umem, &rx_ring, &tx_ring, &cfg));
 						}
-						{
-							uint32_t val = 1;
-							assert(setsockopt(xsk_socket__fd(xsk), SOL_SOCKET, SO_PREFER_BUSY_POLL, &val, sizeof(val)) != -1);
-						}
-						{
-							uint32_t val = 20;
-							assert(setsockopt(xsk_socket__fd(xsk), SOL_SOCKET, SO_BUSY_POLL, &val, sizeof(val)) != -1);
-						}
-						{
-							uint32_t val = 64;
-							assert(setsockopt(xsk_socket__fd(xsk), SOL_SOCKET, SO_BUSY_POLL_BUDGET, &val, sizeof(val)) != -1);
+						if (__IOSUB_BUSY_POLL_SOCK) {
+							{
+								uint32_t val = 1;
+								assert(setsockopt(xsk_socket__fd(xsk), SOL_SOCKET, SO_PREFER_BUSY_POLL, &val, sizeof(val)) != -1);
+							}
+							{
+								uint32_t val = 20;
+								assert(setsockopt(xsk_socket__fd(xsk), SOL_SOCKET, SO_BUSY_POLL, &val, sizeof(val)) != -1);
+							}
+							{
+								uint32_t val = 64;
+								assert(setsockopt(xsk_socket__fd(xsk), SOL_SOCKET, SO_BUSY_POLL_BUDGET, &val, sizeof(val)) != -1);
+							}
 						}
 
 						setup_core_id++;
